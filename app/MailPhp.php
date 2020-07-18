ab1eaa8da014b4536dbe95dfdb2c98dbe63bc1ce
AliAh <m.aliahmed0@gmail.com>
2020-07-18 17:01:50 +0500

    mail fix

--- app/MailPhp.php
+++ app/MailPhp.php
@@ -21,7 +21,7 @@ class MailPhp extends  Model
         try {
             //Server settings
             $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-            $mail->isSMTP();                                            // Send using SMTP
+//            $mail->isSMTP();                                            // Send using SMTP
             $mail->Host = 'smtp.gmail.com';                    // Set the SMTP server to send through
             $mail->SMTPAuth = true;                                   // Enable SMTP authentication
             $mail->Username = 'Medipanda101@gmail.com';                     // SMTP username
