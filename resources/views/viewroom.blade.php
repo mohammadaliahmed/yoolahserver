08eb6f1509ade4f1bb373aae80b9adf6e9be4d3f
Muhammad Ali Ahmed/Tech-IT/Lahore <Ali.Ahmed@wateen.com>
2020-08-26 14:06:18 +0500

    a

--- resources/views/viewroom.blade.php
+++ resources/views/viewroom.blade.php
@@ -18,7 +18,7 @@
                                 {{--<img width="250px" src="../public/qr/{{$room->qr_code}}">--}}
                                 <h2>Room Name: {{$room->title}}</h2>
                                 <br>
-                                <h4>Room Code: {{$room->roomCode}}</h4>
+                                <h4>Room Code: {{$room->roomcode}}</h4>
                                 <br>
                                 <h4>Admin Id to login: {{$admin->randomcode}}</h4>
 
