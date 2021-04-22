42f4b5199cea449bd5646ce17ab1966a099f12c4
Ali AHmed <m.aliahmed0@gmail.com>
2021-04-22 11:31:30 +0500

    logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,7 +27,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    <img width="40" src="{{asset('../storage/app/public/logo.jpg')}}"/>
+                    <img width="40" src="{{asset('../storage/logo.jpg')}}"/>
                     {{ config('app.name', 'Yoolah') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
